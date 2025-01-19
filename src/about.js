135a5c6fd384f2198602a1bfafa57a2a715ab980
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-01-19 22:09:12 +0800

    feat: add about page container and contents Add about page container and contents to content div. Add restaurant and chef images to about page. Add about restaurant text container and ched text container to be filled in later.

--- src/about.js
+++ src/about.js
@@ -1,5 +1,42 @@
+import aboutRestaurantImgPath from "./img/about/about-restaurant.png";
+import chefImgPath from "./img/about/chef.png";
+
 export function getAboutPage() {
     const contentDiv = document.getElementById("content");
     contentDiv.innerHTML = "";
     
+    // Append about-restaurant and chef containers to an about-page container
+    const aboutPageContainer = document.createElement("div");
+    aboutPageContainer.className = "about-page-container";
+    contentDiv.appendChild(aboutPageContainer);
+
+    const aboutRestaurantContainer = document.createElement("div");
+    aboutRestaurantContainer.className = "about-restaurant-container";
+    const chefContainer = document.createElement("div");
+    chefContainer.className = "chef-container";
+
+    aboutPageContainer.appendChild(aboutRestaurantContainer);
+    aboutPageContainer.appendChild(chefContainer);
+
+    // About-restaurant container
+    const restaurantImg = document.createElement("img");
+    restaurantImg.className = "restaurant-img";
+    restaurantImg.src = aboutRestaurantImgPath;
+
+    const aboutTextContainer = document.createElement("div");
+    aboutTextContainer.className = "about-text-container";
+
+    aboutRestaurantContainer.appendChild(restaurantImg);
+    aboutRestaurantContainer.appendChild(aboutTextContainer);
+
+    // Chef container
+    const chefTextContainer = document.createElement("div");
+    chefTextContainer.className = "chef-text-container";
+
+    const chefImg = document.createElement("img");
+    chefImg.className = "chef-img";
+    chefImg.src = chefImgPath;
+
+    chefContainer.appendChild(chefTextContainer);
+    chefContainer.appendChild(chefImg);
 };
