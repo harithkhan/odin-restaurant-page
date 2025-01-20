9650ca1153b7bc28a0780a2b0617eb31d2680d46
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-01-20 11:09:33 +0800

    feat: add contact container to contact page Add contact container to contact page with only the contact img inserted.

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,19 @@
+import contactImgPath from "./img/contact/contact.png";
+import footerIconPath from "./img/github-icon.png";
+
 export function getContactPage() {
     const contentDiv = document.getElementById("content");
     contentDiv.innerHTML = "";
 
+    //Contact Header
+    const contactContainer = document.createElement("div");
+    contactContainer.className = "contact-container";
+
+    contentDiv.appendChild(contactContainer);
+    
+    //Contact Img
+    const contactImg = document.createElement("img");
+    contactImg.className = "contact-img";
+    contactImg.src = contactImgPath;
+    contactContainer.appendChild(contactImg);
 };
