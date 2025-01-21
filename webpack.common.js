41b02716dfd359cc62582e1022104d1dadbeabb3
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-01-21 11:19:10 +0800

    feat: add reconfigured webpack files Add webpack.common.js, webpack.dev.js and webpack.prod.js to root directory to test webpack merge. Remove webpack.config.js.

--- webpack.common.js
+++ webpack.common.js
@@ -2,20 +2,16 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
+      title: 'Production',
     }),
   ],
   module: {
